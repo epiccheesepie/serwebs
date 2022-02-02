fb312ed3caaea1b7dbe45cc6f04c2aa24f7caa64
epiccheesepie <boo-baa-boo@yandex.ru>
2022-02-02 12:48:22 +0300

    update navigation for mobile

--- src/modules/Navigation/Navigation.tsx
+++ src/modules/Navigation/Navigation.tsx
@@ -1,3 +1,4 @@
+// import clsx from 'clsx';
 import clsx from 'clsx';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
@@ -14,12 +15,25 @@ export const Navigation: FC = () => {
     return (
       <div className={css.navigation}>
         <Scrollable className={css.scrollable}>
-          <Link to='/' className={clsx(css.category, css.gold)}>
+          <Link to='/' className={css.category}>
+            <div className={clsx(css.categoryIcon, css.allIcon)}>
+              <Icon type={Icon.Type.DEVELOPER} />
+            </div>
             <div className={css.categoryName}>
-              На главную
+              Все
             </div>
           </Link>
           {categories.map(category => (
+            <Link key={category.id} to={`/${category.alias}`} className={css.category}>
+              <div className={css.categoryIcon}>
+                <Icon type={category.iconType} />
+              </div>
+              <div className={css.categoryName}>
+                {category.name}
+              </div>
+            </Link>
+          ))}
+                    {categories.map(category => (
             <Link key={category.id} to={`/${category.alias}`} className={css.category}>
               <div className={css.categoryIcon}>
                 <Icon type={category.iconType} />
